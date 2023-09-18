310a5c8e8901ec0bd396b104bd6ab4cb4d49acb4
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-09-18 17:57:28 +0800

    Fixed layout of calculator ui on HTML. Added CSS on calculator to reflect default calculator layout. Added initial button configuration to script to place number buttons in display.

--- script.js
+++ script.js
@@ -19,8 +19,41 @@ function operate (a, b, c) {
     return a == "+" ? console.log(add (b, c)) : a == "-" ? 
     console.log(subtract (b, c)) : a == "*" ?
     console.log(multiply (b, c)) : a == "/" ?
-    console.log(divide (b, c)) : "Please try again."
-}
+    console.log(divide (b, c)) : "Please try again.";
+};
 
-operate (operator, numOne, numTwo)
+operate (operator, numOne, numTwo);
+
+
+let display = document.querySelector (".display");
+let btnOne = document.querySelector (".btnOne");
+let btnTwo = document.querySelector (".btnTwo");
+let btnThree = document.querySelector (".btnThree");
+let btnFour = document.querySelector (".btnFour");
+let btnFive = document.querySelector (".btnFive");
+let btnSix = document.querySelector (".btnSix");
+let btnSeven = document.querySelector (".btnSeven");
+let btnEight = document.querySelector (".btnEight");
+let btnNine = document.querySelector (".btnNine");
+let btnZero = document.querySelector (".btnZero");
+let btnDot = document.querySelector (".btnDot");
+let btnDivide = document.querySelector (".btnDivide");
+let btnMultiply = document.querySelector (".btnMultiply");
+let btnMinus = document.querySelector (".btnMinus");
+let btnAdd = document.querySelector (".btnAdd");
+let btnEquals = document.querySelector (".btnEquals");
+
+btnOne.onclick = () => display.textContent = "1";
+btnTwo.onclick = () => display.textContent = "2";
+btnThree.onclick = () => display.textContent = "3";
+btnFour.onclick = () => display.textContent = "4";
+btnFive.onclick = () => display.textContent = "5";
+btnSix.onclick = () => display.textContent = "6";
+btnSeven.onclick = () => display.textContent = "7";
+btnEight.onclick = () => display.textContent = "8";
+btnNine.onclick = () => display.textContent = "9";
+btnZero.onclick = () => display.textContent = "0";
+
+
+
 
