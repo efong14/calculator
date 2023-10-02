4b65cdb026e93d73625d6ef719e36a4c431f5bca
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-10-02 18:41:29 +0800

    Moved variable declartion to top of js file. Changed button onclick for number to add to display instead of change that display to the number via += operator. Added clear button functionality. Added comment for skeleton of object to replace operator, numOne and numTwo variables.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let display = document.querySelector (".display");
+let btnClear = document.querySelector (".btnClear")
+let btnOne = document.querySelector (".btnOne");
+let btnTwo = document.querySelector (".btnTwo");
+let btnThree = document.querySelector (".btnThree");
+let btnFour = document.querySelector (".btnFour");
+let btnFive = document.querySelector (".btnFive");
+let btnSix = document.querySelector (".btnSix");
+let btnSeven = document.querySelector (".btnSeven");
+let btnEight = document.querySelector (".btnEight");
+let btnNine = document.querySelector (".btnNine");
+let btnZero = document.querySelector (".btnZero");
+let btnDot = document.querySelector (".btnDot");
+let btnDivide = document.querySelector (".btnDivide");
+let btnMultiply = document.querySelector (".btnMultiply");
+let btnMinus = document.querySelector (".btnMinus");
+let btnAdd = document.querySelector (".btnAdd");
+let btnEquals = document.querySelector (".btnEquals");
+
 let operator = "*";
 let numOne = 5;
 let numTwo = 5;
@@ -24,35 +43,26 @@ function operate (a, b, c) {
 
 operate (operator, numOne, numTwo);
 
+// display.textContent = ops.operator + ops.numOne + ops.numtwo
+// let ops = {
+//     operator : "*",
+//     numOne : display.textContent,
+//     numtwo : 5
+// };
+// operate (ops.operator, ops.numOne, ops.numTwo);
 
-let display = document.querySelector (".display");
-let btnOne = document.querySelector (".btnOne");
-let btnTwo = document.querySelector (".btnTwo");
-let btnThree = document.querySelector (".btnThree");
-let btnFour = document.querySelector (".btnFour");
-let btnFive = document.querySelector (".btnFive");
-let btnSix = document.querySelector (".btnSix");
-let btnSeven = document.querySelector (".btnSeven");
-let btnEight = document.querySelector (".btnEight");
-let btnNine = document.querySelector (".btnNine");
-let btnZero = document.querySelector (".btnZero");
-let btnDot = document.querySelector (".btnDot");
-let btnDivide = document.querySelector (".btnDivide");
-let btnMultiply = document.querySelector (".btnMultiply");
-let btnMinus = document.querySelector (".btnMinus");
-let btnAdd = document.querySelector (".btnAdd");
-let btnEquals = document.querySelector (".btnEquals");
+btnClear.onclick = () => display.textContent = ""
+btnOne.onclick = () => display.textContent += "1";
+btnTwo.onclick = () => display.textContent += "2";
+btnThree.onclick = () => display.textContent += "3";
+btnFour.onclick = () => display.textContent += "4";
+btnFive.onclick = () => display.textContent += "5";
+btnSix.onclick = () => display.textContent += "6";
+btnSeven.onclick = () => display.textContent += "7";
+btnEight.onclick = () => display.textContent += "8";
+btnNine.onclick = () => display.textContent += "9";
+btnZero.onclick = () => display.textContent += "0";
 
-btnOne.onclick = () => display.textContent = "1";
-btnTwo.onclick = () => display.textContent = "2";
-btnThree.onclick = () => display.textContent = "3";
-btnFour.onclick = () => display.textContent = "4";
-btnFive.onclick = () => display.textContent = "5";
-btnSix.onclick = () => display.textContent = "6";
-btnSeven.onclick = () => display.textContent = "7";
-btnEight.onclick = () => display.textContent = "8";
-btnNine.onclick = () => display.textContent = "9";
-btnZero.onclick = () => display.textContent = "0";
 
 
 
