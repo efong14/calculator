bcc0f8b83b1cbc24986fe306129e3eb38dbdc422
fionicss <118982867+Fionicss@users.noreply.github.com>
2023-10-05 18:22:49 +0800

    Changed number buttons class in html to the same class to reduce redundancy. Used querySelectorAll and forEach on number buttons in js file to reduce redundancy. Created separate function to display first and second set of numbers in operation to create a less bloated onclick function.

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 let display = document.querySelector (".display");
 let btnClear = document.querySelector (".btnClear")
-let btnOne = document.querySelector (".btnOne");
-let btnTwo = document.querySelector (".btnTwo");
-let btnThree = document.querySelector (".btnThree");
-let btnFour = document.querySelector (".btnFour");
-let btnFive = document.querySelector (".btnFive");
-let btnSix = document.querySelector (".btnSix");
-let btnSeven = document.querySelector (".btnSeven");
-let btnEight = document.querySelector (".btnEight");
-let btnNine = document.querySelector (".btnNine");
-let btnZero = document.querySelector (".btnZero");
+let btnNum = document.querySelectorAll (".btnNum");
 let btnDot = document.querySelector (".btnDot");
 let btnDivide = document.querySelector (".btnDivide");
 let btnMultiply = document.querySelector (".btnMultiply");
@@ -49,61 +40,46 @@ function clearBtn () {
     numTwo = 0;
     operator = ""
 };
-function numberBtn (a){
+function clickNum (a){
     if (operator != "") {
-        if (numTwo == 0) {
-            display.textContent = a;
-        } else {
-            display.textContent += a;
-        };
-        numTwo += a;
+        displayTwo (a)
     } else { 
-        if (numOne == 0) {
-            display.textContent = a;
-        } else {
-            display.textContent += a;
-        };
-        numOne += a;
+        displayOne (a)
     }
 };
+
+function displayOne (a) {
+    if (numOne == 0) {
+    display.textContent = a.textContent;
+    } else {
+    display.textContent += a.textContent;
+    };
+    numOne += a;
+}
+function displayTwo (a) {
+    if (numTwo == 0) {
+        display.textContent = a.textContent;
+    } else {
+        display.textContent += a.textContent;
+    };
+    numTwo += a;
+}
+
+
 function opBtn (a){
     display.textContent = a;
     operator = a;
 };
 
 
-// display.textContent = ops.operator + ops.numOne + ops.numtwo
-// let ops = {
-//     operator : "*",
-//     numOne : display.textContent,
-//     numtwo : 5
-// };
-// operate (ops.operator, ops.numOne, ops.numTwo);
-
-
-
 btnClear.onclick = () => clearBtn ()
-btnOne.onclick = () => numberBtn (1);
-btnTwo.onclick = () => numberBtn (2);
-btnThree.onclick = () => numberBtn (3);
-btnFour.onclick = () => numberBtn (4);
-btnFive.onclick = () => numberBtn (5);
-btnSix.onclick = () => numberBtn (6);
-btnSeven.onclick = () => numberBtn (7);
-btnEight.onclick = () => numberBtn (8);
-btnNine.onclick = () => numberBtn (9);
-btnZero.onclick = () => numberBtn (0);
+btnNum.forEach ((button) => button.onclick = () => clickNum (button))
 btnDivide.onclick = () => opBtn ("/")
 btnMultiply.onclick = () => opBtn ("*")
 btnMinus.onclick = () => opBtn ("-")
 btnAdd.onclick = () => opBtn ("+")
 // btnEquals.onclick = () => 
 
-
-
-btnEquals
-
-// function btnOp
 
 
 
